6eb2d46cf4be3dc2631c08670fb7872e59381b7c
Ian Ramos <ianramossantos@hotmail.com>
2021-10-19 15:16:14 -0300

    feat: create controller, service and route for authenticate an user with github

---
+++ node_heat/src/services/AuthenticateUserService.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  login: string;
+  id: number;
+  avatar_url: string;
+  name: string;
+}
+
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = 'https://github.com/login/oauth/access_token';
+
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
+      params: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+      },
+      headers: { Accept: 'application/json' },
+    });
+
+    const response = await axios.get<IUserResponse>('https://api.github.com/user', {
+      headers: { authorization: `Bearer ${accessTokenResponse.access_token}` },
+    });
+
+    return response.data;
+  }
+}
+
+export { AuthenticateUserService };
