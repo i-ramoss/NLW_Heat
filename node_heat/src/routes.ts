527c6e9b4f9d94ec9900c1fafbeb9928641d27c6
Ian Ramos <ianramossantos@hotmail.com>
2021-10-20 13:01:27 -0300

    feat: service to get a user profile

--- node_heat/src/routes.ts
+++ node_heat/src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
 import { GetLastThreeMessagesController } from './controllers/GetLastTreeMessagesController';
+import { ProfileUserController } from './controllers/ProfileUserController';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
 const router = Router();
@@ -10,6 +11,7 @@ const router = Router();
 router.post('/authenticate', new AuthenticateUserController().handle);
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
 
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle);
 router.get('/messages/last3', new GetLastThreeMessagesController().handle);
 
 export { router };
