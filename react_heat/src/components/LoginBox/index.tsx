34e856fb80eeccd67d915fb85ec2842d78c5bd0c
Ian Ramos <ianramossantos@hotmail.com>
2021-10-23 15:56:37 -0300

    feat: create context for user authorization

--- react_heat/src/components/LoginBox/index.tsx
+++ react_heat/src/components/LoginBox/index.tsx
@@ -1,44 +1,12 @@
-import { useEffect } from 'react';
+import { useContext } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc'
 
-import { api } from '../../services/api';
+import { AuthContext } from '../../context/auth';
 
 import styles from './styles.module.scss';
 
-type AuthResponse = {
-  token: string;
-  user: {
-    id: string;
-    avatar_url: string;
-    name: string;
-    login: string;
-  }
-}
-
 export function LoginBox() {
-  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=a60e04c4b103e62518e5`;
-
-  async function signIn(githubCode: string) {
-    const response = await api.post<AuthResponse>('authenticate', { code: githubCode, })
-
-    const { token, user } = response.data;
-
-    localStorage.setItem('@dowhile:token', token);
-  }
-
-  useEffect(() => {
-    const url = window.location.href
-
-    const hasGithubCode = url.includes('?code=');
-
-    if (hasGithubCode) {
-      const [urlWithoutCode, githubCode] = url.split('?code=')
-
-      window.history.pushState({}, '', urlWithoutCode);
-
-      signIn(githubCode);
-    }
-  }, []);
+  const { signInUrl, user } = useContext(AuthContext);
 
   return (
     <div className={styles.loginBoxWrapper}>
